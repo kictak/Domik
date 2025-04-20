e859f2c9a48c7bef2bc677869b0681c27d6d160b
Степан Поляков <step.rhjn@gmail.com>
2025-04-20 21:17:49 +0700

    Добавлены комантарии

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -15,68 +15,68 @@ namespace ConsoleApp1
             context.Database.Migrate();
             return;
 
-            var documentTYPE = new DocumentType()
-            {
-                DocumentName = "tst"
-            };
-            var guest = new Guest()
-            {
-                Name = "Stepan",
-                LastName = "Tramp",
-                GenderId = 1,
-                Adress = "Pushino 36,kv 30",
-                DateOfBirth = new DateOnly(2201, 01, 01),
-                DocumentNumber = "7777",
-                DocumentType = documentTYPE,
-            };
+            //var documentTYPE = new DocumentType()
+            //{
+            //    DocumentName = "tst"
+            //};
+            //var guest = new Guest()
+            //{
+            //    Name = "Stepan",
+            //    LastName = "Tramp",
+            //    GenderId = 1,
+            //    Adress = "Pushino 36,kv 30",
+            //    DateOfBirth = new DateOnly(2201, 01, 01),
+            //    DocumentNumber = "7777",
+            //    DocumentType = documentTYPE,
+            //};
 
 
-            context.Guests.Add(guest);
-
-            var roomtype = new RoomType()
-            {
-                Type = "Luxe",
-                Places = 2
-            };
-
-            var stuff = new Stuff()
-            {
-                Name = "Svetlana",
-                LastName = "Karapuzova",
-                ShiftNumber = 2
-            };
-
-            context.Rooms.Add(new Room() //добавление нового гостя
-            {
-                Number = 42,
-                RoomType = roomtype,
-                DayPrice = 100
-            });
-            context.Stuffs.Add(stuff);
-
-            
             //context.Guests.Add(guest);
 
-            context.SaveChanges();
+            //var roomtype = new RoomType()
+            //{
+            //    Type = "Luxe",
+            //    Places = 2
+            //};
 
-            using (var DbContext = new MyDbContext())
-            {
-                var genderCount = DbContext.Genders.Count();
-                var newGender = DbContext.Genders.FirstOrDefault();
-                if (genderCount == 0)
-                {
-                    context.Genders.Add(new Gender() //добавление нового гендера
-                    {
-                        Name = "male",
-                    });
-                    context.Genders.Add(new Gender() //добавление нового гендера
-                    {
-                        Name = "feemale",
-                    });
-                    context.SaveChanges();
-                    Console.WriteLine("Gender group has been updated.");
-                }
-            }
+            //var stuff = new Stuff()
+            //{
+            //    Name = "Svetlana",
+            //    LastName = "Karapuzova",
+            //    ShiftNumber = 2
+            //};
+
+            //context.Rooms.Add(new Room() //добавление нового гостя
+            //{
+            //    Number = 42,
+            //    RoomType = roomtype,
+            //    DayPrice = 100
+            //});
+            //context.Stuffs.Add(stuff);
+
+            
+            ////context.Guests.Add(guest);
+
+            //context.SaveChanges();
+
+            //using (var DbContext = new MyDbContext())
+            //{
+            //    var genderCount = DbContext.Genders.Count();
+            //    var newGender = DbContext.Genders.FirstOrDefault();
+            //    if (genderCount == 0)
+            //    {
+            //        context.Genders.Add(new Gender() //добавление нового гендера
+            //        {
+            //            Name = "male",
+            //        });
+            //        context.Genders.Add(new Gender() //добавление нового гендера
+            //        {
+            //            Name = "feemale",
+            //        });
+            //        context.SaveChanges();
+            //        Console.WriteLine("Gender group has been updated.");
+            //    }
+            //}
         }
     }
 }
